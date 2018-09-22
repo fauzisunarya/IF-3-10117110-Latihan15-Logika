b0b389fc3c7820c0b7a8c09e5a1697081a876ff6
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-09-22 17:21:00 +0700

    first commit

---
+++ src/pkgif/pkg3/pkg10117110/latihan15/logika/IF310117110Latihan15Logika.java
@@ -0,0 +1,25 @@
+
+package pkgif.pkg3.pkg10117110.latihan15.logika;
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * true or false menggunakan boolean
+ */
+public class IF310117110Latihan15Logika {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+ 
+    }
+    
+}
